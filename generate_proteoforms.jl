2b45e6965a458aed61f8a58528357fd163ca1d7e
James Cobley <j_cobley@yahoo.com>
2024-10-13 15:22:04 +0100

    Update generate_proteoforms.jl

--- generate_proteoforms.jl
+++ generate_proteoforms.jl
@@ -6,14 +6,16 @@ function generate_proteoforms(r::Int)
     return proteoforms
 end
 
-# Function to display the generated proteoforms in order and print the total number
+# Function to display the generated proteoforms in order and print k_value and percentage oxidation
 function display_proteoforms(r::Int)
     proteoforms = generate_proteoforms(r)
     println("Generated proteoforms for r = $r:")
     
-    # Print each proteoform in order
+    # Print each proteoform in order, along with its k_value and percentage oxidation
     for (i, pf) in enumerate(proteoforms)
-        println("PF$(lpad(i, 3, '0')): $pf")
+        k_value = count(c -> c == '1', pf)  # Count the number of '1's (oxidized cysteines)
+        percent_ox = 100 * k_value / r  # Calculate the percentage of oxidation
+        println("PF$(lpad(i, 3, '0')): $pf, k_value: $k_value, Percent Oxidation: $percent_ox%")
     end
     
     # Print the total number of proteoforms
@@ -24,5 +26,5 @@ end
 println("Enter the number of cysteines (r):")
 r = parse(Int, readline())
 
-# Display the proteoforms for the given r
+# Display the proteoforms for the given r, along with k_value and percentage oxidation
 display_proteoforms(r)
