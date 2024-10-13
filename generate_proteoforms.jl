16b52ed22a995195bb8294f39d68a4ca1f919ae2
James Cobley <j_cobley@yahoo.com>
2024-10-13 14:49:48 +0100

    Update generate_proteoforms.jl

--- generate_proteoforms.jl
+++ generate_proteoforms.jl
@@ -59,10 +59,9 @@ function generate_transitions(proteoform_dict::Dict{String, String}, r::Int)
     # Generate allowed and barred transitions for each proteoform
     allowed = [join(find_allowed_transitions(proteoform_dict, PF[i], r), ", ") for i in 1:num_states]
 
-    # Ensure no empty keys in allowed transitions by filtering out any empty substrings
+    # Convert substrings to strings using string(x) for barred transitions
     allowed_clean = [join(filter(x -> x != "", split(allowed[i], ", ")), ", ") for i in 1:num_states]
-
-    barred = [join(find_barred_transitions(PF[i], split(allowed_clean[i], ", "), PF), ", ") for i in 1:num_states]
+    barred = [join(find_barred_transitions(PF[i], [string(x) for x in split(allowed_clean[i], ", ")], PF), ", ") for i in 1:num_states]
 
     # Calculate K - 0 and K + for transitions
     K_minus_0 = [sum(k < k_value[i] for k in [count(c -> c == '1', proteoform_dict[pf]) for pf in split(allowed_clean[i], ", ")]) for i in 1:num_states]
@@ -131,4 +130,3 @@ end
 
 # Call the function to run in terminal
 run_in_terminal()
-
