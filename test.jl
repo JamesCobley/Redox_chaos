0c513f06755a754aec0ea03214faf5a74ac7fd69
James Cobley <j_cobley@yahoo.com>
2024-11-26 16:24:46 +0000

    Update test.jl

--- test.jl
+++ test.jl
@@ -66,7 +66,7 @@ end
 
 # Main function
 function main()
-    file_path = "https://github.com/JamesCobley/Redox_chaos/blob/main/proteoforms_r3.xlsx"  # Adjust this to your actual file path
+    file_path = "/Users/jamescobley/Redox_chaos/Redox_chaos/James.xlsx"  # Adjust this to your actual file path
     proteoforms = load_proteoforms(file_path)
 
     molecules = 100  # Number of molecules
